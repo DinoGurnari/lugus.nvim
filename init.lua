0f64077eb524a36ab4dffe945d730afd13738b64
Dino GURNARI <dinogurnari@gmail.com>
2025-10-03 19:43:52 +0200

    new file for globals

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+-- Set leader, globals from env vars
+require 'globals'
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
