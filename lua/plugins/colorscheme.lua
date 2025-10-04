4b292c8aa4a4a73a8a5e200c998908ed5ddaa1ef
DinoGurnari <dinogurnari@gmail.com>
2025-10-04 19:09:13 +0200

    which key customisation

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,11 +4,11 @@ return {
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'kepano/flexoki-neovim',
+  'Shatur/neovim-ayu',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     ---@diagnostic disable-next-line: missing-fields
-    require('flexoki').setup {
+    require('ayu').setup {
       styles = {
         comments = { italic = false }, -- Disable italics in comments
       },
@@ -17,6 +17,6 @@ return {
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'flexoki'
+    vim.cmd.colorscheme 'ayu-dark'
   end,
 }
